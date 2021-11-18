e7da57f5e9ec640e1d8bc13b4806d85ad2bfa9f3
Gary Mazo <mazo@mazo.com>
2021-11-18 01:15:02 +0000

    steps 1 thru 6

--- labs/3_K8sScaleAndUpdate/app.js
+++ labs/3_K8sScaleAndUpdate/app.js
@@ -4,7 +4,7 @@ var hostname = os.hostname();
 var app = express()
 
 app.get('/', function(req, res) {
-  res.send('Hello world from ' + hostname + '! Your app is up and running!\n')
+  res.send('Welcome to ' + hostname + '! Your app is up and running!\n')
 })
 app.listen(8080, function() {
   console.log('Sample app is listening on port 8080.')
